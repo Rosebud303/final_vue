320cb57991ed954e8026c480fe5608c26d223648
Duy Vu <duyv.thanh@gmail.com>
2019-07-30 18:54:44 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{msg}}
+    <Images v-bind:msg="msg"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Images from './components/Images'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Images
+  },
+  data() {
+    return {
+      msg: 'Helloz'
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Images.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="image in allImages" class="all-images">
+      <p class='rendition'>Rendition Number: {{image.renditionnumber}}</p>
+      <img class='image' v-bind:src="image.baseimageurl"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import apikey from '../../api'
+
+export default {
+  name: "Images",
+  props: ["msg"],
+
+  data() {
+    return {
+      allImages: []
+    }
+
+  },
+  methods: {
+
+  },
+  mounted() {
+    fetch(`https://api.harvardartmuseums.org/image?apikey=${apikey}`)
+      .then(response => response.json())
+      .then(result => this.allImages = result.records)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
